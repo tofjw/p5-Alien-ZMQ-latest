81733928b9f927ac12bf9588177bf899cf2b3cee
unknown <tttfjw@gmail.com>
2019-12-17 23:39:39 +0900

    fix broken example.

--- lib/Alien/ZMQ/latest.pm
+++ lib/Alien/ZMQ/latest.pm
@@ -19,7 +19,7 @@ You can use environment variable to specify ZeroMQ version to be installed.
 Following example can build latest ZeroMQ of version 3.x.
 (Windows and Strawberry perl is intented here)
 
-    > set ALIEN_ZMQ_wVERSION=3.*
+    > set ALIEN_ZMQ_VERSION=3.*
     > perl Makefile.PL
     > gmake
     > gmake install
